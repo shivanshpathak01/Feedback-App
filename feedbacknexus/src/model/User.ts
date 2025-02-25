dd8dd6307d417d909035dd2d697af348ee4025b2
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-02-25 19:58:24 +0530

    User Schema and Message schema done

--- feedbacknexus/src/model/User.ts
+++ feedbacknexus/src/model/User.ts
@@ -1,4 +1,75 @@
 import mongoose,{ Schema, Document } from "mongoose";
 
+export interface Message extends Document{
+    content:string;
+    createdAt: Date
+}
 
-// 7:01
+const MessageSchema: Schema<Message> = new Schema({
+    content:{
+        type:String,
+        required:true
+    },
+
+    createdAt:{
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean; 
+    isAcceptingMessage: boolean;
+    messages: Message[]
+}
+
+
+    const UserSchema: Schema<User> = new Schema({
+        username:{
+            type:String,
+            required:[true, "Username is required"],
+            trim: true,
+            unique: true
+        },
+    
+        email:{
+            type: String,
+            required: [true, "Email is required"],
+            unique:true,
+            match: [/^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,7}$/, "Please use a valid email address"]
+        },
+        password:{
+            type: String,
+            required: [true, "Password is required"],
+        },
+        verifyCode:{
+            type: String,
+            required: [true, "Verify Code is required"],
+        },
+        verifyCodeExpiry:{
+            type: Date,
+            required: [true, "Verify Code Expiry is required"],
+        },
+
+        isVerified:{
+            type: Boolean,
+            required: false,
+        },
+        isAcceptingMessage:{
+            type: Boolean,
+            required: true,
+        },
+        messages: [MessageSchema]
+    })
+
+    // Basically returning mongoose model as we are expecting that we have model in database already not creating first time 
+    // The datatype of the model is <User>
+    const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+    export default UserModel;
