824a093ab2d8c095f67e58a47fd036fb75f841fa
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-04-07 18:19:30 +0000

    ++

--- feedbacknexus/src/app/api/check-username-unique/route.ts
+++ feedbacknexus/src/app/api/check-username-unique/route.ts
@@ -4,29 +4,46 @@ import { z } from 'zod'
 import { usernameValidation } from "@/schemas/signUpSchema";
 
 const UsernameQuerySchema = z.object({
-    username:usernameValidation
+    username: usernameValidation
 })
 
-export async function GET(request: Request){
+export async function GET(request: Request) {
     await dbConnect()
 
     try {
-        const {searchParams} = new URL(request.url)
+        const { searchParams } = new URL(request.url)
         const queryParam = {
-            username:searchParams.get('username')
+            username: searchParams.get('username')
         }
         // validate with Zod
         const result = UsernameQuerySchema.safeParse(queryParam)
-        console.log(result)
-        if(!result.success){
+        // console.log(result)
+        if (!result.success) {
             const usernameErrors = result.error.format().username?._errors || []
             return Response.json({
                 success: false,
                 message: usernameErrors?.length > 0
-                ? usernameErrors.join(',')
-                : 'Invalid query Parameters',
-            }, {status:400})
+                    ? usernameErrors.join(',')
+                    : 'Invalid query Parameters',
+            }, { status: 400 })
         }
+
+        const { username } = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true });
+
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: 'Username is already taken',
+            }, { status: 400 })
+        }
+
+        return Response.json({
+            success: true,
+            message: 'Username is unique',
+        }, { status: 400 })
+
 
     } catch (error) {
         console.error("Error Checking username", error)
@@ -35,7 +52,7 @@ export async function GET(request: Request){
                 success: false,
                 message: "Error checking username"
             },
-            {status : 500}
+            { status: 500 }
         )
     }
 }
